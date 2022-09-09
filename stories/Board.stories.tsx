6337ba787a3391b32b758c6dd961546cd739baef
Mitchell <mitchellwong833@gmail.com>
2022-09-09 18:46:40 +1200

    Improved animations and storybook action/interactions

--- stories/Board.stories.tsx
+++ stories/Board.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { within, userEvent } from '@storybook/testing-library';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Board from '../components/Board';
 
@@ -9,7 +10,35 @@ export default {
 
 const Template: ComponentStory<typeof Board> = (args : any) => <Board {...args} />;
 
-export const GameBoard = Template.bind({});
-GameBoard.args =  {
-    squares: null,
+export const ExampleWinConfig1 = Template.bind({});
+ExampleWinConfig1.play = async ({ canvasElement }) => {
+  for (let i = 0; i < 7; i++) {
+    const canvas = within(canvasElement);
+    const square = canvas.queryAllByRole("button")[i];
+    await userEvent.click(square);
+  }
+}
+
+export const ExampleWinConfig2 = Template.bind({});
+ExampleWinConfig2.play = async ({ canvasElement }) => {
+  for (let i = 0; i < 8; i += 3) {
+    const canvas = within(canvasElement);
+    const square1 = canvas.queryAllByRole("button")[i];
+    await userEvent.click(square1);
+    if (i < 6) {
+      const square2 = canvas.queryAllByRole("button")[i + 1];
+      await userEvent.click(square2);
+    }
+  }
+}
+
+export const ExampleWinConfig3 = Template.bind({});
+ExampleWinConfig3.play = async ({ canvasElement }) => {
+  for (let i = 0; i < 6; i += 2) {
+    const canvas = within(canvasElement);
+    const square1 = canvas.queryAllByRole("button")[i / 2];
+    await userEvent.click(square1);
+    const square2 = canvas.queryAllByRole("button")[i + 3];
+    await userEvent.click(square2);
+  }
 }
--- stories/Square.stories.tsx
+++ stories/Square.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { within, userEvent } from '@storybook/testing-library';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Square from '../components/Square';
 
@@ -6,14 +7,27 @@ export default {
   title: 'Game Components/TicTacToe Grid',
   component: Square,
   argTypes: {
-    onClick: { action: "this is a button" }
+    onClick: { action: "Button Clicked!"}
   }
 } as ComponentMeta<typeof Square>;
 
 const Template: ComponentStory<typeof Square> = (args : any) => <Square {...args} />;
 
-export const GameSquare = Template.bind({});
-GameSquare.args =  {
-    value: null,
-    winner: null,
+export const XSquare = Template.bind({});
+XSquare.args =  {
+    value: "X",
+}
+
+export const OSquare = Template.bind({});
+OSquare.args =  {
+    value: "O",
+}
+
+export const clickableSquare = Template.bind({});
+
+clickableSquare.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByRole("button", {
+    name: ""
+  }));
 }
